291dfdf1414ca1fb9e58dd63760d503598bd55e2
androidblog <androidblog@yandex.ru>
2022-12-06 12:20:04 +0400

    Set the main page to navigate between contacts and appointments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
-import { BrowserRouter, Route, RedirectFunction, NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes, NavLink, Navigate } from 'react-router-dom';
+import AppointmentsPage from './containers/appointmentPage/AppointmentPage';
+import ContactPage from './containers/contactPage/ContactPage';
+
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
   const ROUTES = {
     CONTACTS: "/contacts",
     APPIONTMENTS: "/appointments",
@@ -9,8 +14,24 @@ function App() {
 
   return (
     <>
+        <BrowserRouter>
       <nav>
+        <NavLink to={ROUTES.CONTACTS} className={({ isActive }) => isActive ? "active" : undefined}>
+          Contacts
+        </NavLink>
+        <NavLink to={ROUTES.APPIONTMENTS} className={({ isActive }) => isActive ? "active" : undefined}>
+          Appointments
+        </NavLink>
       </nav>
+      <main>
+          <Routes>          
+            <Route exact path='/' element={<Navigate replace to={ROUTES.CONTACTS} />} />
+            <Route path={ROUTES.CONTACTS} element={<ContactPage />} />
+            <Route path={ROUTES.APPIONTMENTS} element={<AppointmentsPage />} />
+            <Route path='/*' element={<Navigate replace to={ROUTES.CONTACTS}/>} />
+          </Routes>
+      </main>
+      </BrowserRouter>
     </>
   );
 }
